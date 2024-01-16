4b72dbf2eb657e76fe22b97125dfac0dc43976e3
jeonhyoungmin <jhmin7509@gmail.com>
2024-01-17 04:53:44 +0900

    #6 - Data Rest 설정
    유저, 채팅방, 채팅의 json api 를 자동으로 restful 하게 만들게끔 설정

--- src/main/java/com/surup/websocket_chat/repository/UserAccountRepository.java
+++ src/main/java/com/surup/websocket_chat/repository/UserAccountRepository.java
@@ -2,6 +2,8 @@ package com.surup.websocket_chat.repository;
 
 import com.surup.websocket_chat.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
 }
