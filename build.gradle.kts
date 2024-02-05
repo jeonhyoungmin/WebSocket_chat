689df5b538fe1d27ea8ba595a823a65da2f1a417
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-06 00:25:03 +0900

    #17 - DI 추가
    websocket 구현 테스트를 위해 다음의 DI 추가
    
    * WebSocket
    * Spring Security
    * Thymleaf
    
    view에서 websocket 통신 요청을 테스트하기 위해 thymleaf 를 추가하였다.
    또한, thymleaf spring security 를 함께 받기 위해서 spring security 의존성을 추가하였다.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,12 +28,17 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-data-rest")
     implementation("org.springframework.data:spring-data-rest-hal-explorer")
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
