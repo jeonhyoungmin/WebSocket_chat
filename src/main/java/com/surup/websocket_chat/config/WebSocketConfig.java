19c748e5d2c8fd0599af0333630429bcda29db88
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-06 00:31:56 +0900

    #17 - websocket stomp 설정
    stomp 방식의 websocket 설정을 하였다.
    
    * `/chat` websocket 통신 요청 uri
    * `/sub` 구독 요청 uri prefix
    * `/pub` 발행 요청 uri prefix

---
+++ src/main/java/com/surup/websocket_chat/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.surup.websocket_chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker // csrf 방어 기능 포함
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chat")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+}
