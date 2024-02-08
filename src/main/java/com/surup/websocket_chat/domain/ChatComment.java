d49f5396038eb3ac308d1d201796b58e923a58ea
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-09 01:09:32 +0900

    #19 - domain 필드 재설정
    불필요한 데이터를 제거하고 nickname 값을 추가하였다.

--- src/main/java/com/surup/websocket_chat/domain/ChatComment.java
+++ src/main/java/com/surup/websocket_chat/domain/ChatComment.java
@@ -2,6 +2,7 @@ package com.surup.websocket_chat.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -9,34 +10,32 @@ import java.util.Objects;
 
 @Getter
 @ToString
-@Table(indexes = {
+@NoArgsConstructor
+/*
+    TODO : 사용 여부 검토
+    @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
-        @Index(columnList = "createdBy")
-})
+     })
+ */
 @Entity
 public class ChatComment extends AuditingFields {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // 채팅 id
 
-    @Setter @ManyToOne(optional = false) private ChatRoom chatRoom; // 채팅방 (id)
-    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 계정 (id)
-    @Setter @Column(updatable = false) private Long parentCommentId; // 부모 채팅 id (채팅 댓글 바로가기 기능)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @Setter @ManyToOne(optional = false) private ChatRoom chatRoom;
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount;
+    @Setter @Column(nullable = false, length = 50) String nickname;
+    @Setter @Column(nullable = false, length = 1000) private String content;
 
-    @Setter @Column(length = 1000) private String content; // 본문
-
-    protected ChatComment() {}
-
-    private ChatComment(ChatRoom chatRoom, UserAccount userAccount, Long parentCommentId, String content) {
+    public ChatComment(ChatRoom chatRoom, UserAccount userAccount, String nickname, String content) {
         this.chatRoom = chatRoom;
         this.userAccount = userAccount;
-        this.parentCommentId = parentCommentId;
+        this.nickname = nickname;
         this.content = content;
     }
 
-    public static ChatComment of(ChatRoom chatRoom, UserAccount userAccount, Long parentCommentId, String content) {
-        return new ChatComment(chatRoom, userAccount, parentCommentId, content);
+    public static ChatComment of(ChatRoom chatRoom, UserAccount userAccount, String nickname, String content) {
+        return new ChatComment(chatRoom, userAccount, nickname, content);
     }
 
     @Override
