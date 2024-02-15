cd834e16d7564b2a6cb22923f8983a44291742a6
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-16 04:44:16 +0900

    #19 - entity column name change
    spring security 의 기본 형태 적용을 위해 user id 의 컬럼명을 user name 으로 변경한다.

--- src/main/java/com/surup/websocket_chat/dto/SendMessageDto.java
+++ src/main/java/com/surup/websocket_chat/dto/SendMessageDto.java
@@ -5,10 +5,10 @@ import com.surup.websocket_chat.constant.MessageType;
 public record SendMessageDto(
         MessageType messageType,
         Long roomId,
-        String userId,
+        String userName,
         String content
 ) {
-    public static SendMessageDto of(MessageType messageType, Long roomId, String userId, String content) {
-        return new SendMessageDto(messageType, roomId, userId, content);
+    public static SendMessageDto of(MessageType messageType, Long roomId, String userName, String content) {
+        return new SendMessageDto(messageType, roomId, userName, content);
     }
 }
