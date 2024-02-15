8cc9c79e0b78ea58239462610cc9b27486b09463
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-16 04:46:17 +0900

    #19 - add test data
    test 용 data sql 을 추가한다.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+-- user_account data
+insert into user_account (id, user_name, user_password, nickname, created_at) values (1, 'id 1', 'ThisIsPassword', '몽총이 1', '2023-06-15 19:48:54');
+insert into user_account (id, user_name, user_password, nickname, created_at) values (2, 'id 2', 'ThisIsPassword', '몽총이 2', '2023-06-15 19:48:54');
+insert into user_account (id, user_name, user_password, nickname, created_at) values (3, 'id 3', 'ThisIsPassword', '몽총이 3', '2023-06-15 19:48:54');
+insert into user_account (id, user_name, user_password, nickname, created_at) values (4, 'id 4', 'ThisIsPassword', '몽총이 4', '2023-06-15 19:48:54');
+insert into user_account (id, user_name, user_password, nickname, created_at) values (5, 'id 5', 'ThisIsPassword', '몽총이 5', '2023-06-15 19:48:54');
+insert into user_account (id, user_name, user_password, nickname, created_at) values (6, 'id 6', 'ThisIsPassword', '몽총이 6', '2023-06-15 19:48:54');
+insert into user_account (id, user_name, user_password, nickname, created_at) values (7, 'id 7', 'ThisIsPassword', '몽총이 7', '2023-06-15 19:48:54');
+insert into user_account (id, user_name, user_password, nickname, created_at) values (8, 'id 8', 'ThisIsPassword', '몽총이 8', '2023-06-15 19:48:54');
+insert into user_account (id, user_name, user_password, nickname, created_at) values (9, 'id 9', 'ThisIsPassword', '몽총이 9', '2023-06-15 19:48:54');
+insert into user_account (id, user_name, user_password, nickname, created_at) values (10, 'id 10', 'ThisIsPassword', '몽총이 10', '2023-06-15 19:48:54');
+insert into user_account (id, user_name, user_password, nickname, created_at) values (11, 'id 11', 'ThisIsPassword', '몽총이 11', '2023-06-15 19:48:54');
+
+-- chat_room data
+insert into chat_room (id, user_id, title, password, count, created_at) values (1, 1, '채팅방 제목 1', '채팅방 비번 1', 1, '2023-06-15 19:48:54');
+insert into chat_room (id, user_id, title, password, count, created_at) values (2, 2, '채팅방 제목 2', '채팅방 비번 2', 1, '2023-06-15 19:48:54');
+insert into chat_room (id, user_id, title, password, count, created_at) values (3, 3, '채팅방 제목 3', '채팅방 비번 3', 1, '2023-06-15 19:48:54');
+insert into chat_room (id, user_id, title, password, count, created_at) values (4, 4, '채팅방 제목 4', '채팅방 비번 4', 1, '2023-06-15 19:48:54');
+insert into chat_room (id, user_id, title, password, count, created_at) values (5, 5, '채팅방 제목 5', '채팅방 비번 5', 1, '2023-06-15 19:48:54');
+insert into chat_room (id, user_id, title, password, count, created_at) values (6, 6, '채팅방 제목 6', '채팅방 비번 6', 1, '2023-06-15 19:48:54');
+insert into chat_room (id, user_id, title, password, count, created_at) values (7, 7, '채팅방 제목 7', '채팅방 비번 7', 1, '2023-06-15 19:48:54');
+insert into chat_room (id, user_id, title, password, count, created_at) values (8, 8, '채팅방 제목 8', '채팅방 비번 8', 1, '2023-06-15 19:48:54');
+insert into chat_room (id, user_id, title, password, count, created_at) values (9, 9, '채팅방 제목 9', '채팅방 비번 9', 1, '2023-06-15 19:48:54');
+insert into chat_room (id, user_id, title, password, count, created_at) values (10, 10, '채팅방 제목 10', '채팅방 비번 10', 1, '2023-06-15 19:48:54');
+insert into chat_room (id, user_id, title, password, count, created_at) values (11, 11, '채팅방 제목 11', '채팅방 비번 11', 1, '2023-06-15 19:48:54');
