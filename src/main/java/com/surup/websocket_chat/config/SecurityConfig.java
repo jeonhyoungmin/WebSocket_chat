322aba40a6c438ca593e64a8894d0704b4234a05
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-22 00:20:19 +0900

    #19 - update security config
    * static file 접근 모두 허용
    * 비로그인 요청 허용 범위 설정
    * 로그인, 로그아웃 기본 설정
    * UserDetailsService 설정하여 사용자 인증 기능 부여
    * PasswordEncoder 설정

--- src/main/java/com/surup/websocket_chat/config/SecurityConfig.java
+++ src/main/java/com/surup/websocket_chat/config/SecurityConfig.java
@@ -1,10 +1,17 @@
 package com.surup.websocket_chat.config;
 
+import com.surup.websocket_chat.service.UserAccountService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,11 +22,48 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeRequests(authorize -> authorize
-                        .anyRequest().permitAll()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers(
+                                "/",
+                                "/chatRoomList",
+                                "/login",
+                                "/signup"
+                        ).permitAll()
+                        .anyRequest().authenticated()
                 )
-                .formLogin(form -> form.loginPage("/login"))
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/chatRoomList")
+                )
+                // TODO: 문제 해결 필요 - chatRoom(접근 제어) -> login page redirect -> login 시도 -> 실패 -> 재시도 -> 성공
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true)
+                )
                 .httpBasic(withDefaults())
                 .build();
     }
+
+    /*
+        더이상 권장되지 않는 설정
+        @Bean
+        public WebSecurityCustomizer webSecurityCustomizer() {
+            return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        }
+    */
+
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountService userAccountService) {
+        return username -> userAccountService
+                .searchUser(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다 - username: " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
