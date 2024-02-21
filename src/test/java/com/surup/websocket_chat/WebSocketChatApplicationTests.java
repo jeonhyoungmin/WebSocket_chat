16076a7bcf937299ae705c8fde427327e3fc36e6
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-22 00:25:23 +0900

    #19 - update annotaion
    주석 업데이트

--- src/test/java/com/surup/websocket_chat/WebSocketChatApplicationTests.java
+++ src/test/java/com/surup/websocket_chat/WebSocketChatApplicationTests.java
@@ -1,13 +1,16 @@
 package com.surup.websocket_chat;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Disabled
+@DisplayName("애플리케이션")
 @SpringBootTest
 class WebSocketChatApplicationTests {
 
+    @DisplayName("컨텍스트 로드")
     @Test
     void contextLoads() {
     }
