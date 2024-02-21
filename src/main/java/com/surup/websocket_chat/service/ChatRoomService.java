e65f00e7cd3bf34a843e780a390d3eb16b505f4e
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-22 01:01:16 +0900

    #19 - update ChatRoomService
    * 채팅방 id, password 유효성 검사 기능 추가.
    * 채팅방 생성 기능 추가.

--- src/main/java/com/surup/websocket_chat/service/ChatRoomService.java
+++ src/main/java/com/surup/websocket_chat/service/ChatRoomService.java
@@ -1,30 +1,54 @@
 package com.surup.websocket_chat.service;
 
+import com.surup.websocket_chat.constant.ErrorCode;
+import com.surup.websocket_chat.domain.ChatRoom;
+import com.surup.websocket_chat.domain.UserAccount;
 import com.surup.websocket_chat.dto.ChatRoomDto;
+import com.surup.websocket_chat.dto.request.ChatRoomPasswordRequest;
+import com.surup.websocket_chat.dto.request.CreateChatRoomRequest;
+import com.surup.websocket_chat.dto.response.ChatRoomResponse;
+import com.surup.websocket_chat.exception.BaseException;
 import com.surup.websocket_chat.repository.ChatRoomRepository;
+import com.surup.websocket_chat.repository.UserAccountRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@Service
 @Transactional
+@Service
 public class ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
+    private final UserAccountRepository userAccountRepository;
 
-    public List<ChatRoomDto> getChatRoomList() {
-        return chatRoomRepository.findAll()
+    @Transactional(readOnly = true)
+    public List<ChatRoomResponse> getChatRoomList(String searchValue, Pageable pageable) {
+        return chatRoomRepository
+                .findByTitleContainingIgnoreCaseOrUserAccount_NicknameContainingIgnoreCase(searchValue, searchValue, pageable)
                 .stream()
                 .map(ChatRoomDto::from)
+                .map(ChatRoomResponse::from)
                 .toList();
     }
 
-    // TODO:입력된 id 와 password 와 일치하는 데이터 찾기
-    public void isValid(Long id, String password) throws Exception {
-        chatRoomRepository.findByIdAndPassword(id, password)
-                .orElseThrow(() -> new Exception("채팅방이 존재하지 않거나 비밀번호가 유효하지 않습니다"));
+    @Transactional(readOnly = true)
+    public void isValid(ChatRoomPasswordRequest chatRoomPasswordRequest) throws BaseException {
+        chatRoomRepository.findByIdAndPassword(chatRoomPasswordRequest.id(), chatRoomPasswordRequest.password())
+                .orElseThrow(() -> new BaseException(ErrorCode.VALIDATION_ERROR));
     }
+
+    public ChatRoomDto createChatRoom(CreateChatRoomRequest createChatRoomRequest, Long id) {
+        try {
+            UserAccount userAccount = userAccountRepository.findById(id).get();
+            return ChatRoomDto.from(chatRoomRepository.save(
+                    ChatRoom.of(userAccount, createChatRoomRequest.title(), createChatRoomRequest.password())));
+        } catch (Exception e) {
+            throw new BaseException(ErrorCode.DATA_ACCESS_ERROR, e);
+        }
+    }
+
 }
