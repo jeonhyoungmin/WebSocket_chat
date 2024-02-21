6e471b408fb2ba030e342953bc7765c6d7853ece
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-22 00:58:56 +0900

    #19 - update search
    채팅방 제목과 사용자 닉네임으로 검색한 결과를 보여주는 기능 추가.

--- src/main/java/com/surup/websocket_chat/repository/ChatRoomRepository.java
+++ src/main/java/com/surup/websocket_chat/repository/ChatRoomRepository.java
@@ -1,14 +1,14 @@
 package com.surup.websocket_chat.repository;
 
 import com.surup.websocket_chat.domain.ChatRoom;
-import com.surup.websocket_chat.dto.ChatRoomDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findByIdAndPassword(Long id, String password);
+
+    Page<ChatRoom> findByTitleContainingIgnoreCaseOrUserAccount_NicknameContainingIgnoreCase(String title, String nickname, Pageable pageable);
 }
