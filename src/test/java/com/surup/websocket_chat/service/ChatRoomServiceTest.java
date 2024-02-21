c7921312dcb5d762f0b98a16457d8311770f9fe6
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-22 01:02:36 +0900

    #19 - impl chat room test
    채팅방 관련 테스트이다. 추가적인 테스트 필요하다.

--- src/test/java/com/surup/websocket_chat/service/ChatRoomServiceTest.java
+++ src/test/java/com/surup/websocket_chat/service/ChatRoomServiceTest.java
@@ -1,19 +1,22 @@
 package com.surup.websocket_chat.service;
 
 import com.surup.websocket_chat.dto.ChatRoomDto;
+import com.surup.websocket_chat.dto.response.ChatRoomResponse;
 import com.surup.websocket_chat.repository.ChatRoomRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
@@ -28,13 +31,18 @@ class ChatRoomServiceTest {
     @Test
     void givenNothing_whenRequestChatRoomList_thenReturnsChatRoomList() {
         // Given
-        given(chatRoomRepository.findAll()).willReturn(List.of());
+        String searchValue = "채팅";
+        Pageable pageable = PageRequest.of(0, 10, Sort.by(Sort.Order.desc("createdAt")));
+        given(chatRoomRepository
+                .findByTitleContainingIgnoreCaseOrUserAccount_NicknameContainingIgnoreCase(searchValue, searchValue, pageable))
+                .willReturn(Page.empty());
 
         // When
-        List<ChatRoomDto> chatRoomList = chatRoomService.getChatRoomList();
+        List<ChatRoomResponse> chatRoomList = chatRoomService.getChatRoomList(searchValue, pageable);
 
         // Then
-        assertThat(chatRoomList).asList();
-        then(chatRoomRepository).should().findAll();
+        assertThat(chatRoomList).isEmpty();
+        then(chatRoomRepository).should()
+                .findByTitleContainingIgnoreCaseOrUserAccount_NicknameContainingIgnoreCase(searchValue, searchValue, pageable);
     }
 }
