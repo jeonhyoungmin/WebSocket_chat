2d1e5590c03362f21b6f1aa3f09b5b969e243b68
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-22 00:38:01 +0900

    #19 - update AuthController
    회원 가입 기능을 추가하였다.

--- src/main/java/com/surup/websocket_chat/controller/AuthController.java
+++ src/main/java/com/surup/websocket_chat/controller/AuthController.java
@@ -1,10 +1,18 @@
 package com.surup.websocket_chat.controller;
 
+import com.surup.websocket_chat.dto.request.UserRegisterRequest;
+import com.surup.websocket_chat.service.UserAccountService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class AuthController {
+
+  private final UserAccountService userAccountService;
 
   @GetMapping("/login")
   public String login() {
@@ -14,6 +22,12 @@ public class AuthController {
   @GetMapping("/signup")
   public String signup() {
     return "signup";
+  }
+
+  @PostMapping("/signup")
+  public String signup(@Valid UserRegisterRequest userRegisterDto) {
+    userAccountService.createUser(userRegisterDto);
+    return "login";
   }
 
 }
