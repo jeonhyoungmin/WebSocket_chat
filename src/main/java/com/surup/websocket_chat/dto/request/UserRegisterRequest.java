0cb41ad8d3825b4f942d860aca3bf3ab73799991
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-25 12:43:16 +0900

    #23 - update field validation
    최소 길이 추가, null 제외, 빈 공간 제외 설정 추가한다.

--- src/main/java/com/surup/websocket_chat/dto/request/UserRegisterRequest.java
+++ src/main/java/com/surup/websocket_chat/dto/request/UserRegisterRequest.java
@@ -1,12 +1,14 @@
 package com.surup.websocket_chat.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 // TODO: 최소값, 패턴 설정 필요
 public record UserRegisterRequest(
-        @Size(max = 50) String username,
-        @Size(max = 100) String password,
-        @Size(max = 50) String nickname
+        @Size(max = 50, min = 5) @NotBlank String username,
+        @Size(max = 100, min = 5) @NotBlank String password,
+        @Size(max = 50, min = 2) @NotBlank String nickname
 ) {
     public static UserRegisterRequest of(String username, String password, String nickname) {
         return new UserRegisterRequest(username, password, nickname);
