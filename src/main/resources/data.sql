c79fc40fa20bb2ca1ae769b9d96c00ca1dce7972
jeonhyoungmin <jhmin7509@gmail.com>
2024-02-27 17:05:05 +0900

    #25 - update remove chat room insert
    임시 채팅방 데이터 주석 처리한다.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,15 +13,15 @@ insert into user_account (id, user_name, user_password, nickname, created_at) va
 insert into user_account (id, user_name, user_password, nickname, created_at) values (12, 'test', '{noop}test', '몽총이 12', '2023-06-20 19:48:54');
 
 -- chat_room data
-insert into chat_room (id, user_id, title, password, count, created_at) values (1, 1, '채팅방 제목 1', '채팅방 비번 1', 1, '2023-06-15 19:48:54');
-insert into chat_room (id, user_id, title, password, count, created_at) values (2, 2, '채팅방 제목 2', '채팅방 비번 2', 1, '2023-06-16 19:48:54');
-insert into chat_room (id, user_id, title, password, count, created_at) values (3, 3, '채팅방 제목 3', '채팅방 비번 3', 1, '2023-06-17 19:48:54');
-insert into chat_room (id, user_id, title, password, count, created_at) values (4, 4, '채팅방 제목 4', '채팅방 비번 4', 1, '2023-06-18 19:48:54');
-insert into chat_room (id, user_id, title, password, count, created_at) values (5, 5, '채팅방 제목 5', '채팅방 비번 5', 1, '2023-06-19 19:48:54');
-insert into chat_room (id, user_id, title, password, count, created_at) values (6, 6, '채팅방 제목 6', '채팅방 비번 6', 1, '2023-06-20 19:48:54');
-insert into chat_room (id, user_id, title, password, count, created_at) values (7, 7, '채팅방 제목 7', '채팅방 비번 7', 1, '2023-06-21 19:48:54');
-insert into chat_room (id, user_id, title, password, count, created_at) values (8, 8, '채팅방 제목 8', '채팅방 비번 8', 1, '2023-06-22 19:48:54');
-insert into chat_room (id, user_id, title, password, count, created_at) values (9, 9, '채팅방 제목 9', '채팅방 비번 9', 1, '2023-06-23 19:48:54');
-insert into chat_room (id, user_id, title, password, count, created_at) values (10, 10, '채팅방 제목 10', '채팅방 비번 10', 1, '2023-06-24 19:48:54');
-insert into chat_room (id, user_id, title, password, count, created_at) values (11, 11, '채팅방 제목 11', '채팅방 비번 11', 1, '2023-06-25 19:48:54');
-insert into chat_room (id, user_id, title, password, count, created_at) values (12, 12, '비번 없는 방', '', 1, '2023-06-25 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (1, 1, '채팅방 제목 1', '채팅방 비번 1', 1, '2023-06-15 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (2, 2, '채팅방 제목 2', '채팅방 비번 2', 1, '2023-06-16 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (3, 3, '채팅방 제목 3', '채팅방 비번 3', 1, '2023-06-17 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (4, 4, '채팅방 제목 4', '채팅방 비번 4', 1, '2023-06-18 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (5, 5, '채팅방 제목 5', '채팅방 비번 5', 1, '2023-06-19 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (6, 6, '채팅방 제목 6', '채팅방 비번 6', 1, '2023-06-20 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (7, 7, '채팅방 제목 7', '채팅방 비번 7', 1, '2023-06-21 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (8, 8, '채팅방 제목 8', '채팅방 비번 8', 1, '2023-06-22 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (9, 9, '채팅방 제목 9', '채팅방 비번 9', 1, '2023-06-23 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (10, 10, '채팅방 제목 10', '채팅방 비번 10', 1, '2023-06-24 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (11, 11, '채팅방 제목 11', '채팅방 비번 11', 1, '2023-06-25 19:48:54');
+-- insert into chat_room (id, user_id, title, password, count, created_at) values (12, 12, '비번 없는 방', '', 1, '2023-06-25 19:48:54');
